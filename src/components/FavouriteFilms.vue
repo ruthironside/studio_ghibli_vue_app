66ac22a2cbb5b1820c0c594bc6e52342c4657be1
Ruth Ironside <ruthironside@live.com>
2020-11-27 15:52:50 +0000

    add Favourite Film functionality

--- src/components/FavouriteFilms.vue
+++ src/components/FavouriteFilms.vue
@@ -1,9 +1,17 @@
 <template>
-  
+  <div id="favourite_films">
+      <h2>Favourite Films</h2>
+      <ul>
+          <li v-for="favouriteFilm in favouriteFilms">{{favouriteFilm.title}}</li>
+      </ul>
+
+  </div>
 </template>
 
 <script>
 export default {
+    name: 'favourite-films',
+    props: ['favouriteFilms']
 
 }
 </script>
