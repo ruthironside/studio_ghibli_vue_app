bc6ee3e61b918e6059d47fb0d67e6cd36a617195
Ruth Ironside <ruthironside@live.com>
2020-11-28 09:23:34 +0000

    created some dummy  film details to use for unit testing

---
+++ tests/unit/films.js
@@ -0,0 +1,17 @@
+export const filmsData = [{
+    "title": "My Neighbor Totoro",
+    "description": "Two sisters move to the country with their father in order to be closer to their hospitalized mother, and discover the surrounding trees are inhabited by Totoros, magical spirits of the forest. When the youngest runs away from home, the older sister seeks help from the spirits to find her."
+    "director": "Hayao Miyazaki",
+    "producer": "Hayao Miyazaki",
+    "release_date": 1988,
+    "rt_score": 93
+},
+{
+    "title": "Porco Rosso",
+    "description": "Porco Rosso, known in Japan as Crimson Pig (Kurenai no Buta) is the sixth animated film by Hayao Miyazaki and released in 1992. You're introduced to an Italian World War I fighter ace, now living as a freelance bounty hunter chasing 'air pirates' in the Adriatic Sea. He has been given a curse that changed his head to that of a pig. Once called Marco Pagot, he is now known to the world as 'Porco Rosso', Italian for 'Red Pig.'"
+    "director": "Hayao Miyazaki",
+    "producer": "Suzuki",
+    "release_date": 1992,
+    "rt_score": 94
+}
+];
